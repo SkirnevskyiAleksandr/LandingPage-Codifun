e18b8bb3ec0450d6de5b6d187b17c31545254ac7
chel13 <sanya-sha2016@ukr.net>
2020-10-25 21:16:40 +0200

    25-10-20

--- main.js
+++ main.js
@@ -1,51 +1,65 @@
-$('.home-slider').owlCarousel({
-  animateOut: 'fadeOut',
-  items:1,
-  loop:true,
-  dots:false,
-  autoplayHoverPause: false,
-  autoplay: true,
-  smartSpeed: 1000,
-})
+ $(".navbar-collapse a").on("click",function(){
+   $(".navbar-collapse").collapse("hide");
+ });
 
-$('.owl-courses').owlCarousel({
-animateOut: 'fadeOut',
-loop: true,
-autoplayHoverPause: false,
-autoplay: true,
-smartSpeed: 1000,
-dots: false,
-nav:true,
-navText: [
-  '<i class="fa fa-angle-left"></i>',
-  '<i class="fa fa-angle-right"></i>'
-],
-responsiveClass: true,
-responsive: {
-0: {
-  items: 1,
-},
- 1000: {
-  items: 3,
-    }
-}
-});
+ $(window).scroll (function(){
+   if ($(".navbar").offset().top>50){
+     $(".navbar-fixed-top").addClass("top-nav-collapse");
+   }else{
+     $(".navbar-fixed-top").removeClass("top-nav-collapse");
+   }
+ });
 
-$('.owl-client').owlCarousel({
-animateOut: 'fadeOut',
-loop: true,
-autoHeight:true,
-autoplayHoverPause: false,
-autoplay: true,
-smartSpeed: 1000,
-responsiveClass: true,
-responsive: {
-0: {
-  items: 1,
-},
-1000: {
-  items: 3,
-}
-}
-});
+ 
+
+// $('.home-slider').owlCarousel({
+//   animateOut: 'fadeOut',
+//   items:1,
+//   loop:true,
+//   dots:false,
+//   autoplayHoverPause: false,
+//   autoplay: true,
+//   smartSpeed: 1000,
+// })
+
+// $('.owl-courses').owlCarousel({
+// animateOut: 'fadeOut',
+// loop: true,
+// autoplayHoverPause: false,
+// autoplay: true,
+// smartSpeed: 1000,
+// dots: false,
+// nav:true,
+// navText: [
+//   '<i class="fa fa-angle-left"></i>',
+//   '<i class="fa fa-angle-right"></i>'
+// ],
+// responsiveClass: true,
+// responsive: {
+// 0: {
+//   items: 1,
+// },
+//  1000: {
+//   items: 3,
+//     }
+// }
+// });
+
+// $('.owl-client').owlCarousel({
+// animateOut: 'fadeOut',
+// loop: true,
+// autoHeight:true,
+// autoplayHoverPause: false,
+// autoplay: true,
+// smartSpeed: 1000,
+// responsiveClass: true,
+// responsive: {
+// 0: {
+//   items: 1,
+// },
+// 1000: {
+//   items: 3,
+// }
+// }
+// });
 
