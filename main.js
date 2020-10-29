643d1a3cfb4ddb148fd0733e7c430db5673bcf79
chel13 <sanya-sha2016@ukr.net>
2020-10-29 19:29:09 +0200

    29-10-20 19-00

--- main.js
+++ main.js
@@ -20,6 +20,30 @@
   smartSpeed: 1000, 
 })
 
+// popular courses
+$('.owl-courses').owlCarousel({
+  animateOut: 'fadeOut',
+  loop: true,
+  autoplayHoverPause: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  dots: false,
+  nav:true,
+  navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+  ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+     1000: {
+      items: 3,
+        }
+    }
+});
+
 // $('.home-slider').owlCarousel({
 //   animateOut: 'fadeOut',
 //   items:1,
