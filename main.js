eca00dd2277bc424b1b858d6638e4c11b2463ad0
chel13 <sanya-sha2016@ukr.net>
2020-10-30 21:35:34 +0200

    30-10-20 21-10

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
   loop: true,
   dots: false,
   autoplayHoverPause: false,
-  autoplay: true,
+  autoplay: false,
   smartSpeed: 1000, 
 })
 
@@ -25,10 +25,10 @@ $('.owl-courses').owlCarousel({
   animateOut: 'fadeOut',
   loop: true,
   autoplayHoverPause: false,
-  autoplay: true,
+  autoplay: false,
   smartSpeed: 1000,
   dots: false,
-  nav:true,
+  nav:false,
   navText: [
       '<i class="fa fa-angle-left"></i>',
       '<i class="fa fa-angle-right"></i>'
@@ -44,45 +44,12 @@ $('.owl-courses').owlCarousel({
     }
 });
 
-// $('.home-slider').owlCarousel({
-//   animateOut: 'fadeOut',
-//   items:1,
-//   loop:true,
-//   dots:false,
-//   autoplayHoverPause: false,
-//   autoplay: true,
-//   smartSpeed: 1000,
-// })
-
-// $('.owl-courses').owlCarousel({
-// animateOut: 'fadeOut',
-// loop: true,
-// autoplayHoverPause: false,
-// autoplay: true,
-// smartSpeed: 1000,
-// dots: false,
-// nav:true,
-// navText: [
-//   '<i class="fa fa-angle-left"></i>',
-//   '<i class="fa fa-angle-right"></i>'
-// ],
-// responsiveClass: true,
-// responsive: {
-// 0: {
-//   items: 1,
-// },
-//  1000: {
-//   items: 3,
-//     }
-// }
-// });
-
 $('.owl-client').owlCarousel({
 animateOut: 'fadeOut',
 loop: true,
 autoHeight:true,
 autoplayHoverPause: false,
-autoplay: true,
+autoplay: false,
 smartSpeed: 1000,
 responsiveClass: true,
 responsive: {
